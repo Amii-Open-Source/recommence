d6e88938f0286e48f98ae56abd73cae37f961ece
andnp <andnpatterson@gmail.com>
2024-07-10 13:18:35 -0600

    test: staging acceptance test

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import shutil
 from recommence.Checkpoint import Checkpoint, CheckpointConfig
 
 class FakeAgent:
@@ -23,6 +24,31 @@ def test_checkpoint1(tmp_path):
 
     chk = Checkpoint(config)
 
+    loaded_agent = chk['agent']
+
+    assert id(original_agent) != id(loaded_agent)
+    assert np.all(original_agent.weights == loaded_agent.weights)
+    assert original_agent.steps == loaded_agent.steps
+
+    chk.remove()
+
+
+def test_checkpoint2(tmp_path):
+    config = CheckpointConfig(
+        save_path=str(tmp_path / 'dest'),
+        staging_path=str(tmp_path / 'stage'),
+    )
+    chk = Checkpoint(config)
+
+    chk['agent'] = lambda: FakeAgent()
+    original_agent = chk['agent']
+    original_agent.weights[0] = 1
+
+    chk.save()
+    del chk
+    shutil.rmtree(tmp_path / 'stage')
+
+    chk = Checkpoint(config)
     loaded_agent = chk['agent']
 
     assert id(original_agent) != id(loaded_agent)
