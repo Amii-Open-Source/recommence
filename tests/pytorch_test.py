e639017fa92ebc623583ecf293f00255b5cbd19d
Golnazmes <gmesbahi.gm@gmail.com>
2024-09-09 11:35:25 -0600

    test: tests if the checkpointing library works well with torch tensors, Fixes #19

---
+++ tests/pytorch_test.py
@@ -0,0 +1,24 @@
+import torch
+import numpy as np
+from recommence.Checkpoint import Checkpoint, CheckpointConfig
+
+
+class Agent:
+    def __init__(self):
+        self._w = torch.tensor(np.ones(25))
+
+
+def test_jax(tmp_path):
+    config = CheckpointConfig(
+        save_path=str(tmp_path),
+    )
+    chk = Checkpoint(config)
+
+    agent = chk.register("agent", Agent)
+    agent._w *= 2
+    chk.save()
+    del agent
+
+    chk = Checkpoint(config)
+
+    assert torch.all(chk["agent"]._w == (torch.tensor(np.ones(25)) * 2))
